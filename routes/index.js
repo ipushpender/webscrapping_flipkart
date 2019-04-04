57e764cd31d3de0f78f88de43a322405ea1b5f60
pushpender <pushpender1239@gmail.com>
2019-04-04 18:55:21 +0530

    flipakrt mobile

--- routes/index.js
+++ routes/index.js
@@ -15,40 +15,24 @@ router.get('/fetch/flipkart/mobile', (error, res, body) => {
     if (!error && response.statusCode == 200) {
       const $ = cheerio.load(body);
       var item = [];
-      $("._3O0U0u ").each((i, el) => {
-        h1 = $(el).find("a").attr('href');
-        im1 = $(el).find("._1Nyybr").attr("src");
-        s1 = $(el).find(".rIHMVr").text();
-        n1 = $(el).find("._3wU53n").text();
-        r1 = $(el).find(".hGSR34").text();
-        rv1 = $(el).find("._38sUEc").text();
-        mem = $(el).find(".vFw0gD .tVe95H:first-child").text();
-        wa1 = $(el).find(".vFw0gD .tVe95H:last-child").text();
-        disp = $(el).find(".vFw0gD .tVe95H:nth-child(2)").text();
-        camera = $(el).find(".vFw0gD .tVe95H:nth-child(3)").text();
-        battery1 = $(el).find(".vFw0gD .tVe95H:nth-child(4)").text();
-        processor = $(el).find(".vFw0gD .tVe95H:nth-child(5)").text();
-        sim = $(el).find(".vFw0gD .tVe95H:nth-child(6)").text();
-        censor = $(el).find(".vFw0gD .tVe95H:nth-child(7)").text();
-        price = $(el).find("._2GcJzG").text();
-        sp = $(el).find("._1vC4OE ").text();
-        discount = $(el).find(".VGWI6T span").text();
-        obj = {
-          title: n1,
-          href: h1,
-          image: im1,
-          display: disp,
-          warranty: wa1,
-          status: s1,
-          rating: r1,
-          review: rv1,
-          memory: mem,
-          price: sp,
-          discount: discount,
-          old_price: price,
-          sim: sim,
-          battery: battery1,
-          camera: camera
+        $("._3O0U0u ").each((i, el) => {
+      obj = {
+          title: $(el).find("._3wU53n").text(),
+          href: $(el).find("a").attr('href'),
+          image: $(el).find("._1Nyybr").attr("src"),
+          display: $(el).find(".vFw0gD .tVe95H:nth-child(2)").text(),
+          warranty: $(el).find(".vFw0gD .tVe95H:last-child").text(),
+          status:  $(el).find(".rIHMVr").text(),
+          rating: $(el).find(".hGSR34").text(),
+          review: $(el).find("._38sUEc").text(),
+          memory: $(el).find(".vFw0gD .tVe95H:first-child").text(),
+          sim: $(el).find(".vFw0gD .tVe95H:nth-child(6)").text(),
+          battery: $(el).find(".vFw0gD .tVe95H:nth-child(4)").text(),
+          camera: $(el).find(".vFw0gD .tVe95H:nth-child(3)").text(),
+          processor:$(el).find(".vFw0gD .tVe95H:nth-child(5)").text(),
+          price: $(el).find("._1vC4OE ").text(),
+          discount: $(el).find(".VGWI6T span").text(),
+          old_price: $(el).find("._2GcJzG").text()
         }
         item.push(obj);
       });
