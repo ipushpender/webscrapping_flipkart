dfdd397e1184f96fc5e3297e416ec10ced4c5033
pushpender <pushpender1239@gmail.com>
2019-04-09 12:48:16 +0530

    Mobile Full data

---
+++ models/mobile_full.js
@@ -0,0 +1,70 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+let MobileFullSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String
+    },
+    max_price: {
+        type: String
+    },
+    disc: {
+        type: String
+    },
+    offer: {
+        type: String
+    },
+    exchange_offer: {
+        type: String,
+        required: true
+    },
+    special_disc: {
+        type: String,
+        required: true
+    },
+    bank_offer: {
+        type: String
+    },
+    Emi: {
+        type: String
+    },
+    warranty: {
+        type: String,
+        required: true
+    },
+    memory: {
+        type: String,
+        required: true
+    },
+    display: {
+        type: String,
+        required: true
+    },
+    camera: {
+        type: String,
+        required: true
+    },
+    battery: {
+        type: String,
+        required: true
+    },
+    processor: {
+        type: String
+    },
+    seller_rate: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+});
+
+module.exports = mongoose.model('MobileFull', MobileFullSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
+let rp = require("request-promise-native");
 let express = require('express');
 let router = express.Router();
 let request = require('request');
 let cheerio = require('cheerio');
 let Mobile = require('../models/mobile');
 let Tshirt = require('../models/tshirt');
+let MobileFull = require('../models/mobile_full');
 /* GET home page. */
-router.get('/fetch/flipkart/mobile', (error, res, body) => {
+router.get('/fetch/flipkart/mobile', (req, res, next) => {
   const options = {
     url: 'https://www.flipkart.com/search?q=mobile%20phones&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off',
     headers: {
@@ -56,7 +58,7 @@ router.get('/fetch/flipkart/mobile', (error, res, body) => {
   request(options, callback);
 });
 
-router.get('/fetch/snapdeal/tshirt', (error, res, body) => {
+router.get('/fetch/snapdeal/tshirt', (req, res, next) => {
   const options = {
     url: 'https://www.snapdeal.com/search?keyword=t%20shirt&santizedKeyword=t+shirt&catId=0&categoryId=0&suggested=true&vertical=p&noOfResults=20&searchState=&clickSrc=suggested&lastKeyword=&prodCatId=&changeBackToAll=true&foundInAll=false&categoryIdSearched=&cityPageUrl=&categoryUrl=&url=&utmContent=&dealDetail=&sort=rlvncy',
     headers: {
@@ -99,5 +101,75 @@ router.get('/fetch/snapdeal/tshirt', (error, res, body) => {
   request(options, callback);
 });
 
+router.get('/fetch/flipkart/mobile/full', (req, res, next) => {
+  const options = {
+    url: 'https://www.flipkart.com/search?q=mobile%20phones&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off',
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+  let data_array = [];
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(body);
+      $("._3O0U0u").each(async (i, el) => {
+        href = $(el).find("a").attr('href');
+        href = 'https://www.flipkart.com' + href;
+        data_array.push(href);
+      });
+      fetchInfo(data_array, [], function (data) {
+        MobileFull.insertMany(data).then((data) => {
+          res.status(201).json({
+            success: true,
+            message: "Success!!",
+            data: data
+          });
+        }).catch((err) => {
+          res.status(500).json({
+            success: false,
+            message: "Error",
+            Error: err
+          });
+        });
+      })
+    }
+  });
+});
+
+function fetchInfo(data, final_resp, callback) {
+  if (data.length) {
+    let options = data.splice(0, 1)[0]
+    request(options, function (error, response, body) {
+      let obj = {};
+      if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(body);
+        obj = {
+          title: $(" ._9E25nV ._35KyD6").text(),
+          rating: $("._38sUEc span span").text(),
+          price: $("._3qQ9m1 ").text(),
+          max_price: $("._1POkHg").text(),
+          disc: $("._2XDAh9").text(),
+          offer: $("._1iCvwn span").text(),
+          exchange_offer: $("._7g_MLT:nth-child(1) li span").text(),
+          special_disc: $("._7g_MLT:nth-child(2) li span").text(),
+          bank_offer: $("._7g_MLT:nth-child(3) li span").text(),
+          Emi: $("._7g_MLT:nth-child(4) li span").text(),
+          warranty: $("._3h7IGd").text(),
+          memory: $("._3WHvuP ul li:nth-child(1)").text(),
+          display: $("._3WHvuP ul li:nth-child(2)").text(),
+          camera: $("._3WHvuP ul li:nth-child(3)").text(),
+          battery: $("._3WHvuP ul li:nth-child(4)").text(),
+          processor: $("._3WHvuP ul li:nth-child(5)").text(),
+          seller_rate: $(".YddkNl").text(),
+          description: $("._3u-uqB").text()
+        }
+      }
+      final_resp.push(obj)
+      fetchInfo(data, final_resp, callback)
+    })
+  } else {
+    callback(final_resp)
+  }
+}
 
 module.exports = router;
